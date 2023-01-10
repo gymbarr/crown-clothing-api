275cc77eb1ba3a476b808301cb302c1b51a485de
Andrey Timakhovich <stewart96.at@gmail.com>
2023-01-10 14:50:29 +0300

    add getting current user by token action, tests for it and policies

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -8,21 +8,21 @@ RSpec.describe UserPolicy do
   context 'when the user is an administrator' do
     let(:user) { create :user, :with_admin_role }
 
-    it { is_expected.to permit_actions(%i[index show create update destroy]) }
+    it { is_expected.to permit_actions(%i[index show create update destroy me]) }
   end
 
   context 'when the user is a basic user' do
     context 'when the user and the record user are the same' do
       let(:user) { user_record }
 
-      it { is_expected.to permit_actions(%i[create show update destroy]) }
+      it { is_expected.to permit_actions(%i[create show update destroy me]) }
       it { is_expected.to forbid_actions(%i[index]) }
     end
 
     context 'when the user and the record user are different' do
       let(:user) { create :user }
 
-      it { is_expected.to permit_actions(%i[create]) }
+      it { is_expected.to permit_actions(%i[create me]) }
       it { is_expected.to forbid_actions(%i[index show update destroy]) }
     end
   end
