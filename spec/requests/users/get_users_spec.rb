70bee54a901668bb92a68660ff5526c646033b74
Andrey <andrey.timakhovich@rubyroidlabs.com>
2023-01-19 17:17:03 +0300

    update tests for requests with pagination

--- spec/requests/users/get_users_spec.rb
+++ spec/requests/users/get_users_spec.rb
@@ -6,18 +6,21 @@ RSpec.describe 'Users', type: :request do
     subject(:get_users_request) { get '/api/users', headers: }
 
     before do
-      create_list :user, 4
+      create_list :user, 21
       get_users_request
     end
 
     context 'when authorized user' do
       let(:user) { create :user, :with_admin_role }
       let(:headers) { user_auth_header(user) }
-      let(:users_json) { JSON.parse(ActiveModelSerializers::SerializableResource.new(User.all).to_json) }
+      let(:users_per_page) { Pagy::DEFAULT[:items] }
+      let(:users_json) do
+        JSON.parse(ActiveModelSerializers::SerializableResource.new(User.first(users_per_page)).to_json)
+      end
 
       it 'returns a valid JSON' do
-        expect(json).to match_array(users_json)
-        expect(json.size).to eq(User.count)
+        expect(json['users']).to match_array(users_json)
+        expect(json['users'].size).to eq(users_per_page)
       end
 
       it 'returns ok status' do
