d562e40525f409104b78829d2f76efbc9de17e4e
Andrey <andrey.timakhovich@rubyroidlabs.com>
2023-01-25 20:44:36 +0300

    add create charge action for charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,16 @@
 class ChargesController < ApplicationController
+  skip_before_action :authorize_request
+  skip_after_action :verify_authorized
+
+  def create
+    payment_intent = Stripe::PaymentIntent.create(
+      amount: params[:amount],
+      currency: 'usd',
+      payment_method_types: ['card']
+    )
+
+    render json: { clientSecret: payment_intent['client_secret'] }, status: :ok
+  rescue Stripe => e
+    render json: { errors: e.messages }, status: :bad_request
+  end
 end
