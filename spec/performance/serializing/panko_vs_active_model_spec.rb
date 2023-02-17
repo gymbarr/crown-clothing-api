0fad1800199a03ba5f34c1b8a1513a4258541080
Andrey <andrey.timakhovich@rubyroidlabs.com>
2023-02-17 18:55:48 +0300

    add tests for comparing panko and active model serializers

---
+++ spec/performance/serializing/panko_vs_active_model_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Serializing performance comparison', type: :performance do
+  context 'when serializing one object' do
+    let(:product) { create(:product) }
+
+    let(:panko_serializing) { PankoSerializers::ProductSerializer.new.serialize(product).symbolize_keys }
+    let(:active_model_serializing) { ActiveModelSerializers::ProductSerializer.new(product).serializable_hash }
+
+    it 'shows that panko is faster' do
+      expect { panko_serializing }.to(perform_faster_than { active_model_serializing })
+    end
+  end
+
+  context 'when serializing many object' do
+    let(:products) { create_list :product, 100 }
+
+    let(:panko_serializing) do
+      Panko::ArraySerializer.new(products,
+                                 each_serializer: PankoSerializers::ProductSerializer)
+    end
+    let(:active_model_serializing) do
+      ActiveModelSerializers::SerializableResource.new(products,
+                                                       each_serializer: ActiveModelSerializers::ProductSerializer)
+    end
+
+    it 'shows that panko is faster' do
+      expect { panko_serializing }.to(perform_faster_than { active_model_serializing })
+    end
+  end
+end
